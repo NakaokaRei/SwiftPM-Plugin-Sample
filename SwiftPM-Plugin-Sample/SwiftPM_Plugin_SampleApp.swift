4e137ded62c5744cd9641b0b850fc759b5ef27f9
NakaokaRei <reideeplearning@gmail.com>
2022-08-19 23:50:05 +0900

    Initial Commit

---
+++ SwiftPM-Plugin-Sample/SwiftPM_Plugin_SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftPM_Plugin_SampleApp.swift
+//  SwiftPM-Plugin-Sample
+//
+//  Created by rei.nakaoka on 2022/08/19.
+//
+
+import SwiftUI
+
+@main
+struct SwiftPM_Plugin_SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
